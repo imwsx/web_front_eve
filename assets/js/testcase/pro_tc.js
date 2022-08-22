e33578302214ada27c13d323751722624852c9d0
wsx <angel2604520513@163.com>
2022-08-23 00:49:11 +0800

    用例管理优化

--- assets/js/testcase/pro_tc.js
+++ assets/js/testcase/pro_tc.js
@@ -1,13 +1,4 @@
 $(function () {
-  // tinymce.init({
-  //   selector: "#content2",
-  //   language: "zh-Hans",
-  // });
-  // tinymce.init({
-  //   selector: "#content3",
-  //   language: "zh-Hans",
-  // });
-
   var layer = layui.layer;
   var form = layui.form;
   var table = layui.table;
@@ -24,6 +15,7 @@ $(function () {
         id: "idTest",
         url: "/my/testcase/tcs",
         parseData: function (res) {
+          resFormat(res);
           return {
             code: res.status,
             msg: res.message,
@@ -65,6 +57,13 @@ $(function () {
               align: "center",
             },
             {
+              field: "tc_module",
+              title: "用例模块",
+              width: 120,
+              edit: "textarea",
+              align: "center",
+            },
+            {
               field: "tc_title",
               title: "用例标题",
               // width: 180,
@@ -78,6 +77,7 @@ $(function () {
               // minWidth: 160,
               edit: "textarea",
               style: "-moz-box-align: start;",
+              hide: true,
             },
             {
               field: "tc_step",
@@ -92,12 +92,50 @@ $(function () {
               edit: "textarea",
             },
             {
+              field: "tc_remark",
+              title: "备注",
+              // width: 180,
+              edit: "textarea",
+              hide: true,
+            },
+            {
+              field: "tc_pr",
+              title: "优先级",
+              width: 80,
+              align: "center",
+            },
+            {
+              field: "edittime",
+              title: "编辑时间",
+              width: 165,
+              align: "center",
+              hide: true,
+            },
+            {
+              field: "tc_status",
+              title: "状态",
+              width: 250,
+              templet: "#radioTpl",
+              align: "center",
+              unresize: true,
+            },
+            {
+              field: "is_lock",
+              title: "是否启用",
+              width: 110,
+              templet: "#checkboxTpl",
+              align: "center",
+              unresize: true,
+              hide: true,
+            },
+            {
               fixed: "right",
               title: "操作",
-              // width: 180,
+              width: 180,
+              toolbar: "#barDemo",
               // minWidth: 180,
               align: "center",
-              toolbar: "#barDemo",
+              unresize: true,
             },
           ],
         ],
@@ -155,12 +193,12 @@ $(function () {
             layer.open({
               title: "编辑",
               type: 1,
-              area: ["60%", "80%"],
+              area: ["50%", "91%"],
               content: $("#tc-add"),
             });
             break;
           case "deleteAll":
-            layer.confirm("确定删除选中产品吗？", function (index) {
+            layer.confirm("确定删除选中用例吗？", function (index) {
               obj.del();
               layer.close(index);
             });
@@ -173,7 +211,7 @@ $(function () {
             break;
           case "default-row":
             table.reload("idTest", {
-              lineStyle: null, // 恢复单行
+              lineStyle: null,
             });
             layer.msg("已设为单行");
             break;
@@ -182,12 +220,27 @@ $(function () {
             break;
         }
       });
-
       //触发单元格工具事件
       table.on("tool(test)", function (obj) {
-        var checkStatus = table.checkStatus("idTest");
-        if (obj.event === "del") {
-          if (checkStatus.data.length !== 1) return layer.msg("请选择一行");
+        if (obj.event === "save") {
+          obj.data.tc_pr = prToNum(obj.data.tc_pr);
+          if (flag1) obj.data.tc_status = tc_status_value;
+          if (flag2) obj.data.is_lock = tc_lock_value;
+          $.ajax({
+            method: "POST",
+            url: "/my/testcase/updatetc",
+            data: obj.data,
+            success: function (res) {
+              if (res.status !== 0) {
+                return layer.msg("保存失败!");
+              } else {
+                layer.msg("保存成功!");
+                initProCateList();
+              }
+            },
+          });
+        } else if (obj.event === "del") {
+          // if (checkStatus.data.length !== 1) return layer.msg("请选择一行");
           layer.confirm("确定删除该产品吗？", function (index) {
             obj.del();
             layer.close(index);
@@ -196,23 +249,42 @@ $(function () {
           layer.open({
             title: "编辑",
             type: 1,
-            area: ["26%", "50%"],
-            content: $("#form-edit"),
+            area: ["50%", "91%"],
+            content: $("#tc-edit"),
             success: function () {
-              $("#form-edit input:first").attr("value", obj.data.id);
-              form.val("form-edit", {
-                pro_model: obj.data.pro_model,
-                pro_name: obj.data.pro_name,
-                pro_status: statusToNum(obj.data.pro_status),
+              $("#tc-edit input:first").attr("value", obj.data.id);
+              form.val("tc-edit", {
+                tc_module: obj.data.tc_module,
+                tc_title: obj.data.tc_title,
+                tc_pre: obj.data.tc_pre,
+                tc_step: obj.data.tc_step,
+                tc_exp: obj.data.tc_exp,
+                tc_remark: obj.data.tc_remark,
+                tc_pr: prToNum(obj.data.tc_pr),
                 edittime: dayjs(new Date()).format("YYYY-MM-DD HH:mm:ss"),
               });
             },
           });
         }
       });
-
       //触发表格复选框选择
       table.on("checkbox(test)", function (obj) {});
+
+      var flag1 = false;
+      var flag2 = false;
+      form.on("radio(statusDis)", function (obj) {
+        flag1 = true;
+        return (tc_status_value = obj.value);
+      });
+
+      form.on("checkbox(lockDis)", function (obj) {
+        flag2 = true;
+        if (obj.elem.checked === true) {
+          return (tc_lock_value = "1");
+        } else {
+          return (tc_lock_value = "0");
+        }
+      });
 
       //触发表格单选框选择
       table.on("radio(test)", function (obj) {});
@@ -237,7 +309,6 @@ $(function () {
       });
       // 行双击事件
       table.on("rowDouble(test)", function (obj) {});
-
       // 单元格编辑事件
       table.on("edit(test)", function (obj) {
         var field = obj.field, //得到字段
@@ -261,36 +332,36 @@ $(function () {
     });
   });
 
-  $("html").on("submit", "#form-add", function (e) {
+  $("html").on("submit", "#tc-add", function (e) {
     e.preventDefault();
 
     $.ajax({
       method: "POST",
-      url: "/my/pro/addcate",
+      url: "/my/testcase/addtc",
       data: $(this).serialize(),
       success: function (res) {
         if (res.status !== 0) {
-          return layer.msg("新增产品类别失败!");
+          return layer.msg("用例添加失败!");
         } else {
-          layer.msg("新增产品类别成功!");
+          layer.msg("用例添加成功!");
           initProCateList();
         }
       },
     });
   });
 
-  $("html").on("submit", "#form-edit", function (e) {
+  $("html").on("submit", "#tc-edit", function (e) {
     e.preventDefault();
 
     $.ajax({
       method: "POST",
-      url: "/my/pro/updatecate",
+      url: "/my/testcase/updatetc",
       data: $(this).serialize(),
       success: function (res) {
         if (res.status !== 0) {
-          return layer.msg("新增产品类别失败!");
+          return layer.msg("用例更新失败!");
         } else {
-          layer.msg("新增产品类别成功!");
+          layer.msg("用例更新成功!");
           initProCateList();
         }
       },
@@ -299,38 +370,29 @@ $(function () {
 
   function resFormat(res) {
     for (let i = 0; i < res.data.length; i++) {
-      res.data[i].pro_status = statusToText(res.data[i].pro_status);
+      res.data[i].tc_pr = prToText(res.data[i].tc_pr);
     }
   }
 
-  function statusToText(key) {
+  function prToText(key) {
     switch (key) {
       case 0:
-        return "在研";
+        return "高";
       case 1:
-        return "验证";
+        return "中";
       case 2:
-        return "转产";
-      case 3:
-        return "在售";
+        return "低";
     }
   }
 
-  function statusToNum(key) {
+  function prToNum(key) {
     switch (key) {
-      case "在研":
+      case "高":
         return 0;
-      case "验证":
+      case "中":
         return 1;
-      case "转产":
+      case "低":
         return 2;
-      case "在售":
-        return 3;
     }
   }
-
-  tinymce.init({
-    selector: "#content",
-    language: "zh-Hans",
-  });
 });
